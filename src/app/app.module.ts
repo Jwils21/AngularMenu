02a70b511857f9435703d3ca1430bc8594aaa405
Jonathan Wilson <jon.wilsond@gmail.com>
2018-07-17 11:49:10 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule
